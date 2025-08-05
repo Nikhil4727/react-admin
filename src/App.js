2705bae8120176fa2524cf322382ed60fcf3e40c
Nikhil Tripathy <nikhiltripathy30403@gmail.com.com>
2025-08-05 14:29:13 +0530

    Added invoice component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import SideBar from "./scenes/global/SideBar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/context";
 // import Bar  from "./scenes/bar";
 // import Form from "./scenes/form";
@@ -29,7 +29,7 @@ function App() {
                 <Route path="/" element={<Dashboard/>}/>
                 <Route path="/team" element={<Team/>}/>
                 <Route path="/contacts" element={<Contacts/>}/>
-                {/* <Route path="/invoices" element={<Invoices/>}/> */}
+                <Route path="/invoices" element={<Invoices/>}/>
                 {/* <Route path="/form" element={<Form/>}/> */}
                 {/* <Route path="/bar" element={<Bar/>}/> */}
                 {/* <Route path="/pie" element={<Pie/>}/> */}
